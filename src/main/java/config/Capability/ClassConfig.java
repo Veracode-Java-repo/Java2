4f3f5d700d89920881693e06c50e323de54fbbde
VigneshD <vignesh.dhakshnamoorthy@cesltd.com>
2022-06-02 11:56:21 +0530

    excel report

--- src/main/java/config/Capability/ClassConfig.java
+++ src/main/java/config/Capability/ClassConfig.java
@@ -1,7 +1,13 @@
 package config.Capability;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.time.LocalTime;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
@@ -12,6 +18,13 @@ import util.Common.BaseUtil;
 import util.Data.PropertiesUtil;
 
 public class ClassConfig extends BaseUtil {
+	
+	FileOutputStream excelOut;
+	XSSFWorkbook resultXbook;
+	XSSFSheet resultSheet;
+	XSSFRow resultRow;
+	XSSFCell resultCell;
+	int counterExcel = 0;
 
 	public void BeforeSuite() {
 		System.out.println(
@@ -26,10 +39,31 @@ public class ClassConfig extends BaseUtil {
 		waitTime = Integer.parseInt(propUtil.getValue("waitTime"));
 		screenShot.cleanFolders();
 		Extent.StartExtentReport();
-
+		try {
+			excelOut = new FileOutputStream(pathRoot.fileFromExcelReport("ExcelResult.xlsx"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		resultXbook = new XSSFWorkbook();
+		resultSheet = resultXbook.createSheet();
+		resultRow = resultSheet.createRow(0);
+		String[] headings = {"TestCaseID", "TestCaseName", "Time", "Status", "Location"};
+		for (int i = 0; i < headings.length; i++) {
+			resultCell = resultRow.createCell(i);
+			resultCell.setCellValue(headings[i]);
+		}
+		++counterExcel;
 	}
 
 	public void AfterMethod(ITestResult result, String testType) {
+		
+		Object[] results = {counterExcel,result.getName(),LocalTime.now(), result.getStatus(), result.getTestClass().getName()};
+		resultRow = resultSheet.createRow(counterExcel);
+		for (int i = 0; i < results.length; i++) {
+			resultCell = resultRow.createCell(i);
+			resultCell.setCellValue(results[i].toString());
+		}
+		++counterExcel;
 		if (result.getStatus() == ITestResult.SUCCESS) {
 			if(BrowserStack.equalsIgnoreCase("on")) {
 				markTestStatus("passed", "Passed : "+result.getName(), Selenium.get().getDriver());
@@ -62,6 +96,12 @@ public class ClassConfig extends BaseUtil {
 
 	public void AfterSuite() {
 		Extent.EndExtentReport();
+		try {
+			resultXbook.write(excelOut);
+			resultXbook.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("\n===============================================\n\nTest Suite Completed");
 	}
 
