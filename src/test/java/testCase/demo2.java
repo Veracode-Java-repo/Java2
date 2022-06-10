481bab29d485744caf0639b1725351ac7e427e01
VigneshD <vignesh.dhakshnamoorthy@cesltd.com>
2022-06-10 14:20:04 +0530

    clean install

--- src/test/java/testCase/demo2.java
+++ src/test/java/testCase/demo2.java
@@ -1,10 +1,13 @@
 package testCase;
 
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import config.TestNG.Author;
+import pages.SauceLabs.LoginPage;
 import util.Base.TestBase;
 import util.Common.SeleniumUtil;
+import util.Common.SeleniumUtil.locatorType;
 
 public class demo2 extends TestBase {
 
@@ -15,8 +18,23 @@ public class demo2 extends TestBase {
 		SeleniumUtil selenium = Selenium(webURL);
 
 		
-		System.out.println(Locators.get("username_xpath"));
-		System.out.println(Locators.get("passwrod_id"));
+		WebElement Username = selenium.getElement(Locators.get("Username_id"));
+		WebElement password = selenium.findBy_ID("password");
+		selenium.Log(selenium.getLocatorString(Username));
+		Username = selenium.refreshElement(Username);
+		selenium.type(Username, "standard_use");
+		selenium.type(password, "secret_sauce");
+		selenium.click(locatorType.ID, "login-button");
+		selenium.pageScreenShot();
+
+		selenium.Log("Direct Login not successfully");
+		selenium.sleep(2);
+
+		selenium.refresh();
+
+		new LoginPage().Login();
+		selenium.sleep(2);
+		selenium.pageScreenShot();
 
 
 	
